31d6975ba5473ca335259a56fcc053813b97c839
Rachel Ruderman <rachel@yembo.ai>
2019-03-17 20:33:13 -0700

    Add Twitter

---
+++ components/twitter/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+import Twitter from 'twitter';
+const {
+    dev_TWITTER_CONSUMER_KEY,
+    dev_TWITTER_CONSUMER_SECRET,
+    dev_TWITTER_ACCESS_TOKEN_KEY,
+    dev_TWITTER_ACCESS_TOKEN_SECRET} = process.env;
+
+const twitter = new Twitter({
+    consumer_key:           dev_TWITTER_CONSUMER_KEY,
+    consumer_secret:        dev_TWITTER_CONSUMER_SECRET,
+    access_token_key:       dev_TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret:    dev_TWITTER_ACCESS_TOKEN_SECRET
+});
+
+export const createTweet = ({status}) => twitter.post('statuses/update', {status});
--- server/index.js
+++ server/index.js
@@ -2,46 +2,19 @@ require('dotenv').config();
 
 import cron     from 'node-cron';
 import express  from 'express'
-import nodemailer from 'nodemailer';
+import {sendEmail} from "../components/email/index";
+import {createTweet} from "../components/twitter/index";
 
 const app = express();
 
-const {EMAIL_ADDRESS, EMAIL_PASSWORD} = process.env;
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: EMAIL_ADDRESS,
-        pass: EMAIL_PASSWORD
-    }
-});
-
-const mailOptions = {
-    from:       EMAIL_ADDRESS,
-    to:         'raquel.rudermano@gmail.com',
-    subject:    'Subject of your email', // Subject line
-    html:       '<p>Your html here</p>'// plain text body
-};
-
-export const sendEmail = ({message}) => {
-    transporter.sendMail(mailOptions, (error, data) => {
-        if (error) {
-            console.log(error)
-        }
-        else {
-            console.log(data)
-        }
-    });
-};
-
-cron.schedule("* * * * * *", () => {
+cron.schedule("* * * * * *", async () => {
     try {
         console.log("running a task every second");
-
-        sendEmail({message: 'success'});
+        await createTweet({status: 'test'});
+        await sendEmail({message: 'success'});
     }
     catch (error) {
-
+        console.log({error})
     }
 
     // every night at midnight
