8d00cf1da98507325af41159cb6e9b9659433dd2
Rachel Ruderman <rachel@yembo.ai>
2019-03-17 20:33:19 -0700

    Refactor email

--- components/email/index.js
+++ components/email/index.js
@@ -1,27 +1,21 @@
-const nodemailer = require('nodemailer');
+require('dotenv').config();
+import nodemailer from 'nodemailer';
 
-const {EMAIL_ADDRESS, EMAIL_PASSWORD} = process.env;
+const {dev_EMAIL_ADDRESS, dev_EMAIL_PASSWORD} = process.env;
 
-const transporter = nodemailer.createTransport({
+const gmailTransporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: EMAIL_ADDRESS,
-        pass: EMAIL_PASSWORD
+        user: dev_EMAIL_ADDRESS,
+        pass: dev_EMAIL_PASSWORD
     }
 });
 
 const mailOptions = {
-    from:       EMAIL_ADDRESS,
+    from:       dev_EMAIL_ADDRESS,
     to:         'raquel.rudermano@gmail.com',
-    subject:    'Subject of your email', // Subject line
-    html:       '<p>Your html here</p>'// plain text body
+    subject:    'Test', // Subject line
+    html:       '<p>Test</p>'// plain text body
 };
 
-export const sendEmail = async ({message}) => {
-    try {
-        await transporter.sendMail(mailOptions);
-    }
-    catch (error) {
-        console.log('Mail error', error);
-    }
-};
+export const sendEmail = ({message}) => gmailTransporter.sendMail(mailOptions);
