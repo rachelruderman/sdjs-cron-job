19ec721361fc60fe1c9fc25b7a270caf1facaaa3
Rachel Ruderman <rachel@yembo.ai>
2019-03-20 02:07:32 -0700

    Do not include today’s meetups in tomorrow’s meetups

--- components/meetup/index.js
+++ components/meetup/index.js
@@ -1,16 +1,21 @@
 import axios from 'axios';
 import {iso8601DateFormatter} from "../../util/iso8601DateFormatter";
 
+const getStartOfDay     = (date) => iso8601DateFormatter(Math.floor(date / dayInMs) * dayInMs);
+const getEndOfDay       = (date) => iso8601DateFormatter(getStartOfDay(date) + dayInMs - 1);
+const dayInMs           = 86400000;
+
 export const getMeetups = ({when}) => {
-    const dayInMs   = 86400000;
-    const tomorrow  = (Date.now() + dayInMs);
-    const week      = (Date.now() + (dayInMs * 6));
+    const today         = Date.now();
+    const tomorrow      = Date.now() + dayInMs;
+    const thisWeek      = Date.now() + (dayInMs * 6);
+    const baseUrl       = 'https://api.meetup.com/sandiegojs/events';
 
     switch (when) {
         case ('tomorrow'):
-            return axios.get(`https://api.meetup.com/sandiegojs/events?no_later_than=${iso8601DateFormatter(tomorrow)}`);
+            return axios.get(`${baseUrl}?no_earlier_than=${getStartOfDay(tomorrow)}&no_later_than=${getEndOfDay(tomorrow)}`);
         case ('week'):
-            return axios.get(`https://api.meetup.com/sandiegojs/events?no_later_than=${iso8601DateFormatter(week)}`);
+            return axios.get(`${baseUrl}?no_earlier_than=${getStartOfDay(today)}&no_later_than=${getEndOfDay(thisWeek)}`);
         default:
             return new Error('Unrecognized parameter passed to getMeetups');
     }
