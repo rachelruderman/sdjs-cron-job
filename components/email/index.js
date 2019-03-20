273c56721584e8ae3a4a9ae3c7f994ae245e28f9
Rachel Ruderman <rachel@yembo.ai>
2019-03-19 22:51:16 -0700

    Use OAuth2 with nodemailer

--- components/email/index.js
+++ components/email/index.js
@@ -1,24 +1,40 @@
 require('dotenv').config();
 import nodemailer from 'nodemailer';
+import { google } from 'googleapis';
 
-const {dev_EMAIL_ADDRESS, dev_EMAIL_PASSWORD} = process.env;
+const OAuth2 = google.auth.OAuth2;
+const {dev_EMAIL_ADDRESS, dev_GMAIL_CLIENT_ID, dev_GMAIL_CLIENT_SECRET, dev_GMAIL_REFRESH_TOKEN} = process.env;
 
-const gmailTransporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: dev_EMAIL_ADDRESS,
-        pass: dev_EMAIL_PASSWORD
+export const sendEmail = async ({message}) => {
+    try {
+        oauth2Client.setCredentials({refresh_token: dev_GMAIL_REFRESH_TOKEN});
+        const oauth2Client = new OAuth2(dev_GMAIL_CLIENT_ID, dev_GMAIL_CLIENT_SECRET, "https://developers.google.com/oauthplayground");
+
+        const {credentials} = await oauth2Client.refreshAccessToken();
+
+        const smtpTransport = nodemailer.createTransport({
+            service:            'gmail',
+            auth: {
+                type:           'OAuth2',
+                user:           dev_EMAIL_ADDRESS,
+                clientId:       dev_GMAIL_CLIENT_ID,
+                clientSecret:   dev_GMAIL_CLIENT_SECRET,
+                refreshToken:   dev_GMAIL_REFRESH_TOKEN,
+                accessToken:    credentials.accessToken
+            }
+        });
+
+        const mailOptions = {
+            from:       dev_EMAIL_ADDRESS,
+            to:         dev_EMAIL_ADDRESS,
+            subject:    message, // Subject line
+            html:       '<p>Test</p>'// plain text body
+        };
+
+        smtpTransport.sendMail(mailOptions);
+        smtpTransport.close();
     }
-});
-
-
-export const sendEmail = ({message}) => {
-    const mailOptions = {
-        from:       dev_EMAIL_ADDRESS,
-        to:         dev_EMAIL_ADDRESS,
-        subject:    message, // Subject line
-        html:       '<p>Test</p>'// plain text body
-    };
-
-    return gmailTransporter.sendMail(mailOptions);
+    catch (error) {
+        process.exit(1);
+    }
 };
